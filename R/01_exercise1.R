1a0c2d63ca85735ebcca6f9b023359b1e96a1cad
ekinkizildas <ekinkizildas@gmail.com>
2025-01-08 18:47:47 +0100

    I uploaded first question

--- R/01_exercise1.R
+++ R/01_exercise1.R
@@ -1,19 +1,20 @@
 library(tidyverse)
 library(ggplot2)
+library(sf)
 
 Sys.setlocale("LC_ALL", "en_US.UTF-8")
 
-setwd("C:/Users/Casper/Desktop/group (1)/group")
+setwd("C:/Users/Casper/Desktop/group (1)/group/Data_Programming_Group_Project.git")
 
 gert::git_pull()
 
 election_data <- read.csv("C:/Users/Casper/Desktop/group (1)/group/Data_Programming_Group_Project.git/group/election_data_short.csv")
 
-big_municipalities <- election_data %>%
+big_municipalities <- election_data |> 
   filter(censo > 100000)
 
 
-big_municipalities<- big_municipalities %>%
+big_municipalities<- big_municipalities |> 
   pivot_longer(
     cols = starts_with("PARTIDO"), 
     names_to = "party",
@@ -34,10 +35,10 @@ big_municipalities <- big_municipalities  %>%
   ))
 
 
-winners <- big_municipalities %>%
-  group_by(codigo_municipio, anno, party) %>%
+winners <- big_municipalities |> 
+  group_by(codigo_municipio, anno, party) |> 
   summarise(total_votes = sum(votes, na.rm = TRUE), .groups = "drop") %>%
-  group_by(codigo_municipio, anno) %>%
+  group_by(codigo_municipio, anno) |> 
   slice_max(total_votes, n = 1)  
 
 print(winners)
@@ -48,6 +49,26 @@ winners_plot <- ggplot(winners, aes(x = anno, fill = party)) +
        x = "Year", y = "Number of Municipalities", fill = "Party") +
   theme_minimal()
 
+
 print(winners_plot)
 
+#maybe we can add regional analysis
+regional_summary <- big_municipalities |> 
+  group_by(codigo_ccaa, party) |> 
+  summarise(total_municipalities = n(), .groups = "drop") |> 
+  arrange(desc(total_municipalities))
+
+
+ggplot(regional_results, aes(x = anno, y = total_municipalities, color = party)) +
+  geom_line(linewidth = 1.2) +
+  facet_wrap(~codigo_ccaa) + 
+  labs(title = "Regional Performance of Parties Over Time",
+       x = "Year", y = "Number of Municipalities", color = "Party") +
+  theme_minimal()
+
+
 gert::git_add(".")
+
+gert::git_commit(message = "I uploaded first question")
+
+gert::git_push()
